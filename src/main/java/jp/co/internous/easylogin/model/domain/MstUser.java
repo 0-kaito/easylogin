e9ea7ebf53808e62aca17d56ac0d067f731d90f9
0-kaito <tanabekaito50@gmail.com>
2024-02-20 15:26:17 +0900

    init

---
+++ src/main/java/jp/co/internous/easylogin/model/domain/MstUser.java
@@ -0,0 +1,12 @@
+package jp.co.internous.easylogin.model.domain;
+
+public class MstUser {
+
+  private int id;
+  
+  private String userName;
+  
+  private String password;
+  
+  private String fullName;
+}
