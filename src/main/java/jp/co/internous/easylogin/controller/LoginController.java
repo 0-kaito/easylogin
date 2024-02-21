7cccc7102f29f3c53ff2242ae799ec894329f96d
0-kaito <tanabekaito50@gmail.com>
2024-02-21 14:37:04 +0900

    20240221_管理者サイトログイン機能の途中

--- src/main/java/jp/co/internous/easylogin/controller/LoginController.java
+++ src/main/java/jp/co/internous/easylogin/controller/LoginController.java
@@ -1,12 +1,22 @@
 package jp.co.internous.easylogin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.internous.easylogin.model.domain.MstUser;
+import jp.co.internous.easylogin.model.mapper.MstUserMapper;
+
+
 
 @Controller
 @RequestMapping("/easylogin")
 public class LoginController {
+	
+	@Autowired
+	private MstUserMapper userMapper; 
 
 	@GetMapping("/")
 	public String index() {
@@ -14,4 +24,21 @@ public class LoginController {
 	}
 	
 	
+	
+	@GetMapping("login")
+	public String login(String userName, String password, Model model) {
+	    MstUser user = userMapper.findByUserNameAndPassword(userName, password);
+
+	    if (user == null) {
+	        model.addAttribute("message", "ゲストさん、ようこそ！");
+	    } else {
+	        model.addAttribute("message", user.getFullName() + "さん、ようこそ！");
+	    }
+
+	    return "login";
+	}
 }
+
+
+
+
